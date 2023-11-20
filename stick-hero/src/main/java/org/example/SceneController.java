96231de9cc18f5f74660bfa688d5690491e6582a
Ananya Sachdev <83594973+ananya-dot@users.noreply.github.com>
2023-11-20 22:51:32 +0530

    mid submission file

--- stick-hero/src/main/java/org/example/SceneController.java
+++ stick-hero/src/main/java/org/example/SceneController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -16,6 +17,8 @@ public class SceneController {
     private Stage stage;
     private Scene scene;
     private Parent root;
+
+    private Button exitButton;
 
     public void switchToMain(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("StartScreen.fxml")));
@@ -48,6 +51,30 @@ public class SceneController {
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+
+
+
+    public void switchToGamePlay(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("GamePlay.fxml")));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToPauseMenu(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("PauseMenu.fxml")));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+    public void exit(ActionEvent event){
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.close();
     }
 
 
--- stick-hero/src/main/java/org/example/StickHero.java
+++ stick-hero/src/main/java/org/example/StickHero.java
@@ -11,42 +11,16 @@ import java.util.Objects;
 public class StickHero extends Application {
     @Override
     public void start(Stage Pstage) throws Exception {
-//        Group root = new Group();
-//        Scene scene = new Scene(root, 800, 550);
+
         Stage stage = new Stage();
 //        stage.setScene(scene);
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("StickHero.fxml"));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("StartScreen.fxml"));
 
 //        FXMLLoader loader = new FXMLLoader(StickHero.class.getResource("stickhero.fxml"));
         Scene scene = new Scene(root);
 //        stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
-//        Parent root = loader.load();
-//
-//        loader.setRoot(new AnchorPane());
-//        Parent root = fxmlLoader.load();
-
-//        Text text = new Text();
-//        text.setText("hemlo");
-//        text.setX(400);
-//        text.setY(50);
-
-//        Image image = new Image();
-
-//        Line line = new Line();
-//        line.setStartX(0);
-//        line.setEndX(800);
-//        line.setStartY(100);
-//        line.setEndY(100);
-
-
-//        root.getChildrenUnmodifiable().add(text);
-//        root.getChildrenUnmodifiable().add(line);
-//        stage.setScene(new Scene(root, 600, 600));
-//        stage.show();
-
-
 
     }
 }
