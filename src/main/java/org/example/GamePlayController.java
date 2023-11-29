8b9887f4191c3184da39b3c47db770b63495716d
ananya-dot <ananyasachdev2005@gmail.com>
2023-11-29 13:23:34 +0530

    stick is falling

--- src/main/java/org/example/GamePlayController.java
+++ src/main/java/org/example/GamePlayController.java
@@ -37,6 +37,7 @@ public class GamePlayController {
     private boolean isMousePressed;
     private Timeline fallTimeline;
 
+    private Timeline moveCharacterTimeline;
     private Timeline growTimeline;
     private long startTime;
     public void switchToPauseMenu(MouseEvent event) throws IOException {
@@ -61,6 +62,7 @@ public class GamePlayController {
 
         stopGrowTimeline();
         startFallTimeline();
+        moveHarry();
         System.out.println("stopped");
     }
 
@@ -106,16 +108,15 @@ public class GamePlayController {
 
         fallTimeline = new Timeline(new KeyFrame(Duration.millis(50), e -> {
 
-            double angle = 90.0;
-            double newX = centerX + radius * Math.cos(angle);
-            double newY = centerY + radius * Math.sin(angle);
 
-            double rotationAngle = Math.toDegrees(Math.atan2(newY - centerY, newX - centerX));
+            double newX = centerX + radius;
+
+            double rotationAngle = Math.toDegrees(Math.atan2(0.0, newX - centerX));
             stick.getTransforms().clear();
             stick.getTransforms().add(new Rotate(rotationAngle, centerX, centerY));
 
             stick.setEndX(newX);
-            stick.setEndY(newY);
+            stick.setEndY(centerY);
 
             if (stick.getEndY() - centerY >= radius) {
                 stopFallTimeline();
@@ -130,6 +131,25 @@ public class GamePlayController {
         if (fallTimeline != null) {
             fallTimeline.stop();
         }
+    }
+
+    private void moveHarry() {
+        moveCharacterTimeline = new Timeline(new KeyFrame(Duration.millis(50), e -> {
+            // Move the character to the end of the stick
+            double characterX = stick.getEndX() - harry.getBoundsInLocal().getWidth() / 2;
+            double characterY = stick.getEndY() - harry.getBoundsInLocal().getHeight() / 2;
+
+            // Ensure the character remains within the visible area
+            characterX = Math.max(0, Math.min(characterX, gamePlayRoot.getWidth() - harry.getBoundsInLocal().getWidth()));
+            characterY = Math.max(0, Math.min(characterY, gamePlayRoot.getHeight() - harry.getBoundsInLocal().getHeight()));
+
+            harry.setX(characterX);
+            harry.setY(characterY);
+        }));
+        moveCharacterTimeline.setCycleCount(Timeline.INDEFINITE);
+
+        // Start the timeline to move the character
+        moveCharacterTimeline.play();
     }
 
 
