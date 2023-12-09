c496b91e12d0f4c429f0359ae8cf9c5e31b199a1
maulik <maulik22282@iiitd.ac.in>
2023-12-09 17:19:07 +0530

    Revert "changing line coordinates"
    This reverts commit bd58fc31bea8a3575d5e0bd2669080509fccbd53.

--- src/main/java/org/example/GamePlayController.java
+++ src/main/java/org/example/GamePlayController.java
@@ -3,8 +3,6 @@ package org.example;
 
 import javafx.animation.*;
 import javafx.application.Platform;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -14,14 +12,11 @@ import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.transform.Rotate;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.util.Duration;
-import java.util.Random;
 
 
 import java.io.IOException;
@@ -37,7 +32,8 @@ public class GamePlayController {
     private ImageView pauseButton;
     @FXML
     private ImageView harry;
-    private Timeline fallCheckTimeline;
+//    private Stick stick;
+private Timeline fallCheckTimeline;
     @FXML
     private Line stick;
     private double lengthOfStick;
@@ -50,11 +46,10 @@ public class GamePlayController {
     @FXML
     private Rectangle pillar1;
     private boolean isGrowing;
-    private ObservableList<Rectangle> Pillars;
+    private CompletableFuture<Void> fallFuture;
 
     private Timeline moveCharacterTimeline;
     private Timeline growTimeline;
-    private Timeline movePillarsTimeline;
     @FXML
     private Button growButton;
     private boolean longEnough;
@@ -65,12 +60,6 @@ public class GamePlayController {
     boolean actionsCompleted;
 
     boolean characterHasFallen;
-    private int currentPillarIndex = 1; // Tracks the current pillar index
-    private Rectangle originalPillar1;
-    private Rectangle originalPillar2;
-
-    private Timeline[] growTimelines;
-
     public void switchToPauseMenu(MouseEvent event) throws IOException {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("PauseMenu.fxml")));
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
@@ -83,12 +72,6 @@ public class GamePlayController {
         gameStatus = true;
 //        growButton.setOnMousePressed(event -> growingActions());
 //        growButton.setOnMouseReleased(event -> stopGrowingActions());
-//        gamePlayRoot.getChildren().remove(pillar1);
-//        gamePlayRoot.getChildren().remove(pillar2);
-//        createPillars();
-//        movePillars();
-        originalPillar1 = pillar1;
-        originalPillar2 = pillar2;
         startGameLoop();
 
 
@@ -103,22 +86,15 @@ public class GamePlayController {
                     return;
                 }
 
-
                 CompletableFuture.runAsync(() -> {
                     Platform.runLater(() -> {
-                        growButton.setVisible(true);
-                        System.out.println("Actions-"+actionsCompleted);
-                        System.out.println("run later");
-                        movePillars();
                         resetStickAndHarry();
                         growButton.setVisible(true);
+                        movePillars();
                     });
                 }).whenComplete((result, throwable) -> {
                     actionsCompleted = false;
-//                    resetStickAndHarry();
-
                 });
-
 
                 if (!gameStatus) {
                     gameLoop.stop();
@@ -140,198 +116,21 @@ public class GamePlayController {
         double durationInMillis = 1000; // Adjust the duration as needed
         double pixelsToMove = diff; // Adjust the number of pixels to move
 
-        movePillarsTimeline = new Timeline(
-                new KeyFrame(Duration.ZERO, new KeyValue(originalPillar1.layoutXProperty(), originalPillar1.getLayoutX())),
+        Timeline movePillarsTimeline = new Timeline(
+                new KeyFrame(Duration.ZERO, new KeyValue(pillar1.layoutXProperty(), pillar1.getLayoutX())),
                 new KeyFrame(Duration.millis(durationInMillis),
-                        new KeyValue(pillar1.layoutXProperty(), originalPillar1.getLayoutX() - pixelsToMove)),
+                        new KeyValue(pillar1.layoutXProperty(), pillar1.getLayoutX() - pixelsToMove)),
                 new KeyFrame(Duration.ZERO, new KeyValue(harry.layoutXProperty(), harry.getLayoutX())),
                 new KeyFrame(Duration.millis(durationInMillis),
                         new KeyValue(harry.layoutXProperty(), harry.getLayoutX() - pixelsToMove)),
-                new KeyFrame(Duration.ZERO, new KeyValue(originalPillar2.layoutXProperty(), originalPillar2.getLayoutX())),
+                new KeyFrame(Duration.ZERO, new KeyValue(pillar2.layoutXProperty(), pillar2.getLayoutX())),
                 new KeyFrame(Duration.millis(durationInMillis),
-                        new KeyValue(originalPillar2.layoutXProperty(), originalPillar2.getLayoutX() - pixelsToMove))
+                        new KeyValue(pillar2.layoutXProperty(), pillar2.getLayoutX() - pixelsToMove))
 
         );
 
-        movePillarsTimeline.setOnFinished(event -> {
-
-//
-//            // After the animation finishes, synchronize pillar1 with pillar2
-//            pillar1.setLayoutX(pillar2.getLayoutX());
-//            pillar1.setLayoutY(pillar2.getLayoutY());
-//            pillar1.setWidth(pillar2.getWidth());
-//            pillar1.setHeight(pillar2.getHeight());
-//            pillar1.setFill(pillar2.getFill());
-
-            System.out.println("p1 X-"+pillar1.getLayoutX());
-            System.out.println("p2 X-"+pillar2.getLayoutX());
-
-            // Update the position of pillar2
-            Random random = new Random();
-            int randomDistance = random.nextInt(3) + 1;
-            double newXPosition = 3 * 100; // Assuming each unit is 100 pixels
-
-            // Update the layout and setX of pillar2
-//            pillar1.setLayoutX(pillar2.);
-//            pillar2.setLayoutX(newXPosition);
-//            pillar2.setLayoutY(pillar1.getLayoutY()); // Align the Y position with pillar1
-
-            // Optionally, you can also update other properties of pillar2 if needed
-
-            // Create a TranslateTransition to slide pillar1 forward to the random distance
-            TranslateTransition slideForward = new TranslateTransition(Duration.seconds(1), pillar1);
-            slideForward.setToX(newXPosition);
-            slideForward.setInterpolator(Interpolator.EASE_BOTH);
-            slideForward.play();
-            System.out.println(newXPosition);
-//            pillar1.setLayoutX(newXPosition+pillar1.getLayoutX());
-
-//            pillar1.setLayoutX(0);
-//            Rectangle temp = pillar1;
-//            pillar1 = pillar2;
-//            pillar2 = temp;
-            pillar1.setX(newXPosition+pillar1.getLayoutX());
-
-
-//            pillar2.setLayoutX(newXPosition);
-//            Rectangle temp = pillar1;
-//            pillar1 = pillar2;
-//            pillar2 = temp;
-//            pillar2.setLayoutX(pillar1.getLayoutX());
-            System.out.println("p1 X-"+pillar1.getLayoutX());
-            System.out.println("p2 X-"+pillar2.getLayoutX());
-
-
-//            resetPillars(pillar1,pillar2);
-
-
-        });
-
         movePillarsTimeline.play();
     }
-
-
-
-    private void stopPillarsTimeline(){
-        if(movePillarsTimeline!=null){
-            movePillarsTimeline.stop();
-        }
-    }
-
-    private void createPillars(){
-        Random random=new Random();
-        Pillars = FXCollections.observableArrayList();
-        Rectangle rect = new Rectangle(60, 160, Color.rgb(176, 129, 97));
-        rect.setLayoutX(-1); // Set layout based on the loop index
-        rect.setLayoutY(240);
-        Pillars.add(rect);
-        gamePlayRoot.getChildren().add(rect);
-
-
-        for(int i=1;i<=5;i++){
-            Rectangle rectangle = new Rectangle(30*(random.nextInt(5)), 160, Color.rgb(176, 129, 97));
-            rectangle.setLayoutX(i * 100); // Set layout based on the loop index
-            rectangle.setLayoutY(240);
-            rectangle.setTranslateY(160);
-            Pillars.add(rectangle);
-            gamePlayRoot.getChildren().add(rectangle);
-        }
-
-    }
-
-//private void movePillars() {
-//    if (currentPillarIndex < Pillars.size()) {
-//        Rectangle currPillar = Pillars.get(0);
-//        // Move the character to the next pillar (You can implement this logic)
-//        TranslateTransition transition = new TranslateTransition(Duration.seconds(2), currPillar);
-//        transition.setToY(0); // Move the pillar to the desired Y position                                            //workable
-//        transition.setCycleCount(1);
-//        transition.play();
-//
-//
-//
-//        // Simulate a delay for the character movement (for demonstration purposes)
-//        PauseTransition pause = new PauseTransition(Duration.seconds(2));
-//        pause.setOnFinished(event -> {
-//            // After the character reaches the pillar, spawn the next pillar
-//            spawnNextPillar();
-//        });
-//        pause.play();
-//    }
-//}
-
-private void spawnNextPillar(){
-    System.out.println("hello pillars");
-    if (currentPillarIndex < Pillars.size()) {
-        Rectangle nextPillar = Pillars.get(currentPillarIndex);
-        double startingY = gamePlayRoot.getHeight(); // Set the starting Y position below the screen
-
-        TranslateTransition transition = new TranslateTransition(Duration.seconds(2), nextPillar);
-        transition.setToY(0); // Move the pillar to the desired Y position                                            //workable
-        transition.setCycleCount(1);
-
-        // Increment the index for the next pillar after the current animation finishes
-        transition.setOnFinished(event -> {
-            currentPillarIndex++;
-            movePillars(); // Move the next pillar
-        });
-
-        // Start the transition animation
-        transition.play();
-    }
-}
-//private void movePillars() {
-//    System.out.println("hello pillars");
-//    if (currentPillarIndex < Pillars.size()) {
-//        Rectangle nextPillar = Pillars.get(currentPillarIndex);
-//        double startingY = gamePlayRoot.getHeight(); // Set the starting Y position below the screen
-//
-//        TranslateTransition transition = new TranslateTransition(Duration.seconds(2), nextPillar);
-//        transition.setToY(0); // Move the pillar to the desired Y position                                            //workable
-//        transition.setCycleCount(1);
-//
-//        // Increment the index for the next pillar after the current animation finishes
-//        transition.setOnFinished(event -> {
-//            currentPillarIndex++;
-//            movePillars(); // Move the next pillar
-//        });
-//
-//        // Start the transition animation
-//        transition.play();
-//    }
-//}
-
-//    if (currentPillarIndex < Pillars.size()) {
-//        Rectangle nextPillar = Pillars.get(currentPillarIndex);
-//        // Move the character to the next pillar (You can implement this logic)
-//
-//        // Simulate a delay for the character movement (for demonstration purposes)
-//        PauseTransition pause = new PauseTransition(Duration.seconds(2));
-//        pause.setOnFinished(event -> {
-//            // After the character reaches the pillar, spawn the next pillar
-//            spawnNextPillar();
-//        });
-//        pause.play();
-//    } else {
-//        movePillars();
-//    }
-//}
-//
-//    private void spawnNextPillar() {
-//        if (currentPillarIndex < Pillars.size()) {
-//            Rectangle nextPillar = Pillars.get(currentPillarIndex);
-//            double startingY = gamePlayRoot.getHeight(); // Set the starting Y position below the screen
-//
-//            TranslateTransition transition = new TranslateTransition(Duration.seconds(2), nextPillar);
-//            transition.setToY(-160); // Move the pillar to the desired Y position
-//            transition.setCycleCount(1);
-//            transition.play();
-//
-//            // Increment the index for the next pillar
-//            currentPillarIndex++;
-//        }
-//    }
-
 
     private void updateGameState() {
         // Perform game-related logic here
@@ -357,17 +156,19 @@ private void spawnNextPillar(){
 
 
     private void resetStickAndHarry() {
-        stick.getTransforms().clear();
-        harry.getTransforms().clear();
-        stick.setStartX(stick.getStartX());
-        stick.setEndX(stick.getStartX());
+         stick.setEndX(0);
+         stick.setEndY(0);
+         stick.setStartX(0);
+         stick.setStartY(0);
+//         harry.setX(0);
+//         harry.setY(0);
+         stick.getTransforms().clear();
+         harry.getTransforms().clear();
+
+
     }
 
-//    public void resetPillars(Rectangle pillarOne,Rectangle pillarTwo){
-//        originalPillar1.setLayoutX(pillarOne.getLayoutX());
-//    }
-
-    //    private void moveHarryToNextPillar() {
+//    private void moveHarryToNextPillar() {
 //        // Example logic to move harry to the next pillar
 //        double nextPillarX = pillar2.getBoundsInParent().getCenterX() + pillar2.getWidth() / 2 + harry.getFitWidth() / 2;
 //        double nextPillarY = pillar2.getBoundsInParent().getMaxY() - harry.getFitHeight();
@@ -384,8 +185,8 @@ private void spawnNextPillar(){
 
 
 
-    public void grow(MouseEvent event) {
-        System.out.println("grow function called");
+        public void grow(MouseEvent event) {
+            System.out.println("grow function called");
         isMousePressed = true;
         growingActions();
 
@@ -396,7 +197,7 @@ private void spawnNextPillar(){
 //        isGrowing = true;
         System.out.println("growing action function called");
 
-//        actionsCompleted = false;
+        actionsCompleted = false;
         startTime = System.currentTimeMillis();
         startGrowTimeline();
 
@@ -425,10 +226,10 @@ private void spawnNextPillar(){
     private void fallHarry() {
 
 
-        double totalDistance = pillar2.getHeight();
+         double totalDistance = pillar2.getHeight();
 //
 
-        fallCharacterTimeline = new Timeline(new KeyFrame(Duration.millis(50), e -> {
+         fallCharacterTimeline = new Timeline(new KeyFrame(Duration.millis(50), e -> {
 
             double moveStep = 5.0;
 
@@ -448,7 +249,6 @@ private void spawnNextPillar(){
                 characterHasFallen=true;
 
             }
-            actionsCompleted = true;
 
 
 
@@ -465,43 +265,7 @@ private void spawnNextPillar(){
 
     }
 
-//    private void fallHarry() {
-//
-//        double moveStep = 5.0;
-//        final int[] currentPillarIndex = {0};
-//
-//        fallCharacterTimeline = new Timeline(new KeyFrame(Duration.millis(50), e -> {
-//            double currentPillarHeight = Pillars.get(currentPillarIndex[0]).getHeight();
-//
-//            if (harry.getY() < currentPillarHeight) {
-//                harry.setY(harry.getY() + moveStep);
-//            } else {
-//                if (currentPillarIndex[0] < Pillars.size() - 1) {
-//                    currentPillarIndex[0]++;
-//                } else {
-//                    // Character has reached the last pillar
-//                    stopFallCharacterTimeline();
-//                    actionsCompleted = true;
-//                    gameStatus = false;
-//                    characterHasFallen = true;
-//                    return;
-//                }
-//            }
-//
-//            double nextPillarHeight = Pillars.get(currentPillarIndex[0]).getHeight();
-//            if (harry.getY() + moveStep >= nextPillarHeight) {
-//                // The character reached the next pillar, stop the fall or handle accordingly
-//                stopFallCharacterTimeline();
-//                // Additional logic after reaching the next pillar
-//            }
-//        }));
-//
-//        fallCharacterTimeline.setCycleCount(Timeline.INDEFINITE);
-//        fallCharacterTimeline.play();
-//
-//    }
-//
-//
+
 
 
 
@@ -515,6 +279,7 @@ private void spawnNextPillar(){
 
         }));
         growTimeline.setCycleCount(Timeline.INDEFINITE);
+//        growTimeline.setAutoReverse(true);
 
         growTimeline.setOnFinished(e -> {
             if (isMousePressed()) {
@@ -530,37 +295,6 @@ private void spawnNextPillar(){
 
         growTimeline.play();
     }
-//private void startGrowTimeline() {
-//    growTimelines = new Timeline[Pillars.size()];
-//    double durationInMillis = 50; // Duration for each cycle in milliseconds
-//
-//    for (int i = 0; i < Pillars.size(); i++) {
-//        Rectangle currentPillar = Pillars.get(i);
-//
-//        growTimelines[i] = new Timeline(new KeyFrame(Duration.millis(durationInMillis), event -> {
-//            stick.setEndY(stick.getEndY() - 5);
-//
-////            currentPillar.setHeight(currentPillar.getHeight() - 5);
-//            // Modify other properties of the rectangle as needed
-//        }));
-//        growTimelines[i].setCycleCount(Timeline.INDEFINITE);
-//
-//        final int index = i;
-//        growTimelines[i].setOnFinished(e -> {
-//            if (isMousePressed()) {
-//                long currentTime = System.currentTimeMillis();
-//                long elapsedTime = currentTime - this.startTime;
-//                int cycles = (int) (elapsedTime / durationInMillis);
-//                growTimelines[index].setCycleCount(cycles);
-//                growTimelines[index].setAutoReverse(false);
-//                growTimelines[index].playFromStart();
-//            }
-//        });
-//
-//        growTimelines[i].play();
-//    }
-//}
-
 
     private void stopGrowTimeline() {
 //        System.out.println("entered stop growing");
@@ -570,15 +304,6 @@ private void spawnNextPillar(){
 
         }
     }
-//private void stopGrowTimeline() {
-//    if (growTimelines != null) {
-//        for (Timeline timeline : growTimelines) {
-//            if (timeline != null) {
-//                timeline.stop();
-//            }
-//        }
-//    }
-//}
 
     private boolean isMousePressed() {
 
@@ -668,13 +393,17 @@ private void spawnNextPillar(){
     }
 
 
+
+
+
+
+
+
     @FXML
     private void moveHarry() {
-        System.out.println("harry moving");
-        System.out.println("pillar1's X:"+pillar1.getX());
-        System.out.println("pillar1's layout X:"+pillar1.getLayoutX());
-        System.out.println("pillar2's X:"+pillar2.getX());
-        System.out.println("pillar2's layout X:"+pillar2.getLayoutX());
+
+//
+
 
         double totalDistance = -1 * stick.getEndY() - harry.getX();
 
@@ -693,8 +422,10 @@ private void spawnNextPillar(){
             }
 
             if(harry.getX() >= totalDistance){
-                stopMoveCharacterTimeline();
-                actionsCompleted = true;
+
+
+            stopMoveCharacterTimeline();
+//            actionsCompleted = true;
             }
         }));
         moveCharacterTimeline.setCycleCount(Timeline.INDEFINITE);
@@ -715,19 +446,15 @@ private void spawnNextPillar(){
     private void initializeFallCheckTimeline() {
 //
 
-        fallCheckTimeline = new Timeline(new KeyFrame(Duration.millis(50), e -> {
+         fallCheckTimeline = new Timeline(new KeyFrame(Duration.millis(50), e -> {
 
             if (harry.getX() >= -1 * lengthOfStick) {
 //                System.out.println("i am in fall check");
                 if(!longEnough){
                     fallHarry();
                     stopFallCheckTimeline();
-                    gameLoop.stop();
                 }
-                else {
-                    actionsCompleted = true;
-                    gameLoop.stop();
-                }
+                else actionsCompleted = true;
 
 
 
@@ -756,3 +483,9 @@ private void spawnNextPillar(){
 
 
 }
+
+
+
+
+
+
